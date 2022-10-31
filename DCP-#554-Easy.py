30de9b40f94091cbbeb18b141882cad3289d4195
obusama <eswaruce@gmail.com>
2022-10-31 23:40:42 +0530

    Egyptian fraction

---
+++ DCP-#554-Easy.py
@@ -0,0 +1,41 @@
+"""
+The ancient Egyptians used to express fractions as sum of several terms where each numerator is one. For Example, 4/13
+can be represented as 1/4+1/18+1/468. Create an algorithm to turn ordinary fraction a/b where a<b into an Egyptian fraction
+
+"""
+"""
+pseudocode
+1. find the largest unit fraction
+2. remove it
+3. for the remaining repeat 1 and 2
+4. end when the final fraction is found
+"""
+import math
+import fractions
+from os import devnull
+
+
+def solution(num,den):
+        
+    if (num<=0 or den<=0):
+        return None
+
+    elif (den%num==0):
+        return int(den/num)
+      
+    else:
+        
+        unit = math.ceil(den/num)
+        return unit
+            
+def egyptian_decimal(num,den):
+    unit = solution(num,den)
+    if unit is not None:
+        numerator = num*unit - den
+        denominator = unit*den
+        return f"+ 1/{unit} "+egyptian_decimal(numerator,denominator)
+    return ""
+
+
+print(egyptian_decimal(4,13))
+
